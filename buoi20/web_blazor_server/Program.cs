f5d084466c1dd84969538a620b032539f77633b4
kienkienkienkienkien <ktran78113@gmail.com>
2025-12-29 11:54:07 +1000

    buoi20 - wip

---
+++ buoi20/web_blazor_server/Program.cs
@@ -0,0 +1,34 @@
+// Tạo ra trang web
+var builder = WebApplication.CreateBuilder(args);
+// ------ build ------
+
+
+// This is the 2 libraries that allow u to run the app on the server 
+
+builder.Services.AddRazorPages(); // DI (thêm) thư viên razor web
+builder.Services.AddServerSideBlazor(); // DI (add) server side library 
+
+
+// Build Trang web
+var app = builder.Build();
+
+
+
+// ------ use ------ use the library 
+
+
+// Web được chạy trên domain https and http 
+// Neey ko dùng cái này thì chỉ mỗi domain http được dùng 
+app.UseHttpsRedirection();
+
+
+
+// Before the app run -> all middleware will be activated first 
+// app.MapFallBackToPage() -> determined which page will be run first 
+
+app.MapBlazorHub(); //middleware -> make file run first
+// --> 
+app.MapFallbackToPage("/_Host"); // first file
+
+// Khi dùng dotnet run -> cái này sẽ được call
+app.Run();
